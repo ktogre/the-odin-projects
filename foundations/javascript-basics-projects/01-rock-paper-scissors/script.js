458d2cd5220e17c47b8d7cf64142ce32acbcb916
ktogre <212162456+ktogre@users.noreply.github.com>
2026-01-03 23:52:21 +0100

    Create the computer choice function This function will randomly return rock, paper or scissors

--- foundations/javascript-basics-projects/01-rock-paper-scissors/script.js
+++ foundations/javascript-basics-projects/01-rock-paper-scissors/script.js
@@ -1,1 +1,19 @@
-console.log("Oh, Humanity!");
+function getComputerChoice(rock, paper, scissors, min, max) {
+    rock = "rock";
+    paper = "paper";
+    scissors = "scissors";
+    min = 1;
+    max = 3;
+
+    let computerChoice = Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (computerChoice === 1) {
+        return rock;
+    } else if (computerChoice === 2) {
+        return paper;
+    } else if (computerChoice === 3) {
+        return scissors;
+    } else {
+        return "Computer didn't make a choice";
+    }
+}
