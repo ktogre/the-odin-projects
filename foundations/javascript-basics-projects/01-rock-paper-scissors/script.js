38c7d4f258e02711938e5fd2960ea8be73620775
ktogre <212162456+ktogre@users.noreply.github.com>
2026-01-04 20:31:58 +0100

    Add the playGame function This function use a loop to call the playRound Function five times and update the human and computer score based on the winner

--- foundations/javascript-basics-projects/01-rock-paper-scissors/script.js
+++ foundations/javascript-basics-projects/01-rock-paper-scissors/script.js
@@ -1,7 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
 
-
 function getComputerChoice(rock, paper, scissors, min, max) {
     rock = "rock";
     paper = "paper";
@@ -27,26 +26,36 @@ function getHumanChoice(humanChoice) {
     return humanChoice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("It's a draw! Play again");
-        return;
-    } else if ((humanChoice === "rock" && computerChoice == "scissors") ||
-        (humanChoice === "scissors" && computerChoice == "paper") ||
-        (humanChoice === "paper" && computerChoice == "rock")) {
-        console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
-        humanScore += 1;
-    } else if ((computerChoice === "rock" && humanChoice == "scissors") ||
-        (computerChoice === "scissors" && humanChoice == "paper") ||
-        (computerChoice === "paper" && humanChoice == "rock")) {
-        console.log("Computer wins! Rock beats Scissors.");
-        computerScore += 1;
-    } else {
-        console.log("Invalid choice! Try again");
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log("It's a draw! Play again");
+            return;
+        } else if ((humanChoice === "rock" && computerChoice == "scissors") ||
+            (humanChoice === "scissors" && computerChoice == "paper") ||
+            (humanChoice === "paper" && computerChoice == "rock")) {
+            console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+            humanScore += 1;
+        } else if ((computerChoice === "rock" && humanChoice == "scissors") ||
+            (computerChoice === "scissors" && humanChoice == "paper") ||
+            (computerChoice === "paper" && humanChoice == "rock")) {
+            console.log("Computer wins! Rock beats Scissors.");
+            computerScore += 1;
+        } else {
+            console.log("Invalid choice! Try again");
+        }
+    }
+
+    for (let i = 1; i <= 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(`Your Score: ${humanScore}
+        Computer Score: ${computerScore}`);
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-let x = playRound(humanSelection, computerSelection);
+playGame();
